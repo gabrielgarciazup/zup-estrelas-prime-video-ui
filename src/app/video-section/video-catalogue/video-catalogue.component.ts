8a93f99f0c91bc9bbe145a1c356bf16329832a44
gabrielgarciazup <gabriel.garcia@zup.com.br>
2021-02-02 15:47:32 -0300

    Estrutura inicial dos componentes

---
+++ src/app/video-section/video-catalogue/video-catalogue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-catalogue',
+  templateUrl: './video-catalogue.component.html',
+  styleUrls: ['./video-catalogue.component.scss']
+})
+export class VideoCatalogueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
